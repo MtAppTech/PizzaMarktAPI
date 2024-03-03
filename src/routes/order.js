ad78672dc53d471501d4aff5402539a838fa57d4
murat <yazilimgenel@gmail.com>
2024-03-03 21:49:57 +0100

    order

--- src/routes/order.js
+++ src/routes/order.js
@@ -7,19 +7,18 @@ const router = require('express').Router()
 // routes/order:
 
 const order = require('../controllers/order')
-const permissions = require('../middlewares/permissions')
 
 // URL: /orders
 
 router.route('/')
-    .get(permissions.isLogin, order.list)
-    .post(permissions.isLogin, order.create)
+    .get(order.list)
+    .post(order.create)
 
 router.route('/:id')
-    .get(permissions.isLogin, order.read)
-    .put(permissions.isLogin, order.update)
-    .patch(permissions.isLogin, order.update)
-    .delete(permissions.isLogin, order.delete)
+    .get(order.read)
+    .put(order.update)
+    .patch(order.update)
+    .delete(order.delete)
 
 /* ------------------------------------------------------- */
 module.exports = router
